ffb620650f11135a20805cb2b456495956e93f66
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-06-27 08:08:51 +0400

    Working authentication in specs

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,17 +1,17 @@
 module ControllerMacros
   def login_admin
     before(:each) do
-      #@request.env["devise.mapping"] = Devise.mappings[:user]
+      request.env["devise.mapping"] = Devise.mappings[:user]
       sign_in Factory.create(:admin)
     end
   end
 
   def login_user
+    let(:user) { Factory.create(:regular_user) }
     before(:each) do
-      #@request.env["devise.mapping"] = Devise.mappings[:user]
-      
-      @user = Factory.create(:regular_user)
-      sign_in @user
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+      request.env['warden'] = mock(Warden, :authenticate => user, :authenticate! => user)
     end
   end
 end
